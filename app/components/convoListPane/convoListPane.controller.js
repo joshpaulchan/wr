0fbc90dfcbb753c6318f9460c2769c622505ba4e
joshpaulchan <joshpaulchan@gmail.com>
2016-11-07 10:53:58 -0500

    Add stubs for convoList functions

--- app/components/convoListPane/convoListPane.controller.js
+++ app/components/convoListPane/convoListPane.controller.js
@@ -1,12 +1,16 @@
-'use strict';
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
 
 // `convoListPane`
 // Controls the convo-list (conversation list pane) in displaying the
-// conversations and do bulk/singular slection, move, and mark opeartions
+// conversations and do bulk/singular book-keeping operations.
 //
 // @pre     : the user must be logged-in
 //
-// @attr    : query             : String            : the search bar query
+// @attr    : query             : String            : the search query
+// @attr    : isSearch          : Boolean           : whether a search is being
+// performed or not
 // @attr    : filters           : Object            : the list of conversation
 // filters
 // @attr    : curFilter         : String            : the current filter being
@@ -16,99 +20,84 @@
 // @attr    : numItemsPerPage   : Number            : the number of
 // conversations to display per page
 // @attr    : convos            : Conversation[]    : the conversation objects
+// @attr    : selectedConvos    : String[]          : the ids of currently
+// selected conversation objects
 //
+
 // @method  : loadConversations : null              : loads the conversations
 // from the server using the conersation service
 
 // @method  : markAs            : null              : mark conversations as
 
-// TODO: add options to mark conversations as
-// TODO: add options to move conversations to
-// TODO: finish adding method stubs
-
 angular.module('wrControllers')
-.controller('convoListPane', function($scope, $location, convoService, errorService) {
+.controller('convoListPane', function($scope, $stateProvider, convoService, notificationService) {
     $scope.query = "";
+    $scope.isSearch = false;
     $scope.filters = {
-        "All"       : "@CONVERSATIONS__FILTER-ALL",
-        "Unread"    : "@CONVERSATIONS__FILTER-UNREAD",
-        "Unreplied" : "@CONVERSATIONS__FILTER-UNREPLIED"
+        "All": "@CONVERSATIONS__FILTER-ALL",
+        "Unread": "@CONVERSATIONS__FILTER-UNREAD",
+        "Unreplied": "@CONVERSATIONS__FILTER-UNREPLIED"
     };
     $scope.curFilter = $scope.filters[Object.keys($scope.filters)[0]];
     $scope.curPage = 0;
     $scope.numItemsPerPage = 25;
     $scope.convos = [];
+    $scope.selectedConvos = [];
 
     // Load messages when initialized
     $scope.$on(convoService.isReady, () => {
-        $scope.loadConversations($scope.curPage);
+        $scope.$apply(() => {
+            $scope.loadConversations($scope.curPage);
+        });
     });
 
-    // `loadConversations(pg)`
-    // Loads the conversations from the server using the conersation service
+    ////////////////// FILTERING AND SEARCHING CONVERSATIONS //////////////////
+
+    // FIXME: `loadConversations(pg)`
+    // Loads the conversations from the server using the conersation service.
     //
     // @pre     : the conversation service must be initialized
-    // @pre     : the user must be logged in
     // @pre     : pg must be a non-negative integer
     // @post    : [success] the numItemsPerPage will saved to the service,
     // allowing `loadNextPage` to be used instead
-    // @post    : [success] the conversations will be sent to the scope
+    // @post    : [success] the conversations will replace the current convos
     // @post    : [error] if no more items (pg > LAST_PAGE), do nothing
     // @post    : [error] if other error, error will be sent to error service to
     // display on page and log for analytics
     //
     // @param   : pg    : Number    : the page number of the converations to get
     // @return  : null
-    $scope.loadConversations = function(pg) {
-        convoService.loadMessages($scope.curPage, $scope.numItemsPerPage)
-        .then(
-            (conversations) => { $scope.convos = conversations; },
-            (err) => {
-            // TODO: implement error service + error notification directive
-            errorService.addError(err);
-        });
+    $scope.loadConversations = (pg) => {
+        convoService
+            .loadConvos($scope.curPage, $scope.numItemsPerPage)
+            .then(
+                (conversations) => {
+                    $scope.convos = conversations;
+                },
+                (err) => {
+                    // TODO: notification service + error notification directive
+                    console.error(err);
+                });
     };
 
-    ///////////////////////////
-    // MESSAGE LIST CONTROLS //
-    ///////////////////////////
-
-    $scope.openMarkModal = () => {
-
-    };
-
-    $scope.openMoveModal = () => {
-
-    };
-
-    $scope.markConvoss = (convoIds, status) => {
-
-    };
-
-    $scope.moveConvoss = (convoIds) => {
-
-    };
-
-    $scope.showConvo = (convoId) => {
-
-    };
-
-    $scope.markAs = function() {
-        //
-    }
-
-    $scope.moveMessageTo = function(message, dest) {
-        message.folder = dest;
-    };
-
-    $scope.byStatus = function(message) {
+    // FIXME: `byStatus(convo)`
+    // Works with `ng-repeat | filter` to filter the shown conversations in the
+    // list.
+    //
+    // @pre     : `convo` should be a valid conversation object to filter
+    // @post    : true will be returned if the convo object meets the current
+    // showing criteria (i.e. is unread, unreplied/all), false otherwise
+    //
+    // @param   : convo     : Conversation  : the conversation item to judge
+    // @return  : Boolean   : whether or not `convo` should be shown
+    $scope.byStatus = (convo) => {
         var flag = true;
         switch ($scope.show) {
             case "Unread":
-                flag = message.status.unread;
+                flag = convo.status.unread;
                 break;
             case "unreplied":
-                flag = !message.status.repliedTo;
+                flag = !convo.status.repliedTo;
                 break;
             case "All":
                 break;
@@ -116,48 +105,199 @@ angular.module('wrControllers')
         return flag;
     };
 
-    $scope.disableDropdown = function() {
-        var selectedMessages = $scope.getSelected();
-        var curMessage = messages.getCurMessage();
-        return (selectedMessages === 0 && curMessage === null);
+    // FIXME: `loadConversations(pg)`
+    // Loads the conversations from the server using the conersation service.
+    //
+    // @pre     : the conversation service must be initialized
+    // @pre     : pg must be a non-negative integer
+    // @post    : [success] the numItemsPerPage will saved to the service,
+    // allowing `loadNextPage` to be used instead
+    // @post    : [success] the conversations will replace the current convos
+    // @post    : [error] if no more items (pg > LAST_PAGE), do nothing
+    // @post    : [error] if other error, error will be sent to error service to
+    // display on page and log for analytics
+    //
+    // @param   : pg    : Number    : the page number of the converations to get
+    // @return  : null
+    $scope.searchConvos = () => {
+
     };
 
-    ////////////////////////////////
-    // MESSAGE LIST-ITEM CONTROLS //
-    ////////////////////////////////
+    // SELECTING CONVERSATIONS
 
-    $scope.getSelected = function() {
-        return $scope.messages.filter(function(message) {
-            return (message.status.selected);
-        });
+    // TODO: `toggleSelect(convoId)`
+    // Selects (or de-selects) an item depending on whether it is currently
+    // being selected.
+    //
+    // @pre     : `convoId` must be a valid Conversation id
+    // @pre     : `ev` must be a valid Event object
+    // @post    : if convo is not selected, `convoId` will be selected (put into
+    // the the array of selected convos)
+    // @post    : if convo is selected, `convoId` will be de-selected and
+    // removed from the array of selected convos
+    //
+    // @param   : convoId   : the id of the conversation to select
+    // @param   : ev        : event object that triggered this
+    // @return  :
+    $scope.toggleSelect = (convoId, ev) => {
+        ev.stopPropagation();
+        // CODE IN HERE
+        // FIXME: how to save selected convos between conversation view?
     };
 
-    $scope.curMessage = messages.curMessage;
-    $scope.forwardPressed = false;
-
-    $scope.$watch(function() {
-        return ($scope.curMessage !== messages.curMessage);
-    }, function(newMessage, oldMessage, scope) {
-        $scope.curMessage = messages.curMessage;
-        $scope.forwardPressed = false;
-    });
-    $scope.clearSelected = function() {
-        $scope.messages = $scope.messages.map(function(message, i) {
-            message.status.selected = false;
-            return (message);
-        });
+    // TODO: `selectAll()`
+    // Selects all the current conversations in scope.
+    //
+    // @pre     : null
+    // @post    : all the conversations shown in the list will be selected
+    //
+    // @param   : null
+    // @return  : null
+    $scope.selectAll = () => {
+        // CODE IN HERE
     };
 
-    $scope.viewMessage = function(message) {
-        // 1. set is as new message
-        if (messages.getCurMessage() !== message) {
-            messages.setCurMessage(message);
-        } else {
-            messages.setCurMessage(null);
-        }
+    // TODO: `deselectAll()`
+    // De-Selects all the conversations currently selected.
+    //
+    // @pre     : null
+    // @post    : all currently selected conversations will be de-selected.
+    //
+    // @param   : null
+    // @return  : null
+    $scope.deselectAll = () => {
+        // CODE IN HERE
+    };
 
-        // 2. change path
-        var pathId = (!!messages.getCurMessage()) ? ("/" + message.id) : "";
-        $location.path("/messages" + pathId, false);
+    // TODO: `listButtonsDisabled()`
+    // Checks whether the conversation list items shoudl be disabled or not.
+    //
+    // @pre     : null
+    // @post    : to be true, at least one conversation should be view or
+    // selected
+    // @post    : to be false, not a single conversation must be "open" or
+    // selected
+    //
+    // @param   : null
+    // @return  : Boolean   : whether the list buttons should be active or not
+    $scope.enableListButtons = () => {
+        // CODE IN HERE
+    };
+
+    ////////////////////////// MARKING CONVERSATIONS //////////////////////////
+
+    // TODO: `openMarkModal()`
+    // Opens the 'Mark As' modal for marking a conversation/several
+    // conversations as a certain status (i.e. read, replied).
+    //
+    // @pre     : at least one conversation must be selected
+    // @pre     : no other modal (including the Mark modal) should be open
+    // @post    : [success] the modal will be 'opened' and visible on screen
+    //
+    // @param   : null
+    // @return  : null
+    $scope.openMarkModal = () => {
+        // CODE IN HERE
+    };
+
+    // TODO: `closeMarkModal()`
+    // Closes the 'Mark As' modal for marking a conversation/several
+    // conversations as a certain status (i.e. read, replied).
+    //
+    //
+    // @post    : [success] the Mark modal will become invisible and inactive
+    //
+    // @param   : null
+    // @return  : null
+    $scope.closeMarkModal = () => {
+        // CODE IN HERE
+    };
+
+    // TODO: `markConvos(convoIds, status)`
+    // Uses convoService to mark the given conversations (convoIds) with the
+    // given status (i.e. read/unread, unreplied/replied).
+    //
+    // @pre     : `convoIds` must be an array of valid conversation ids
+    // @pre     : `status` must be a valid string corresponding to a status a
+    // conversation can be marked as
+    // @post    : [success] the conversations will be set to the given status
+    // @post    : [error] the conversations will all retain their current status
+    // @post    : [error] a notification will be logged with the error
+    //
+    // @param   : convoIds  : String[]  : id(s) of conversations to change
+    // the status of
+    // @param   : status    : String    : the status to change the
+    // conversation(s) to
+    // @return  : null
+    var markConvos = (convoIds, status) => {
+        // CODE IN HERE
+    };
+
+    /////////////////////////// MOVING CONVERSATIONS ///////////////////////////
+
+    // TODO: `openMoveModal()`
+    // Opens the 'Move To' modal for selecting a folder (i.e inbox, trash, etc.)
+    // to move a conversation/several conversations to.
+    //
+    // @pre     : at least one conversation must be selected
+    // @pre     : no other modal (even the Move modal) should be open on screen
+    // @post    : [success] the Mark modal will be opened and visible on screen
+    //
+    // @param   : null
+    // @return  : null
+    $scope.openMoveModal = () => {
+        // CODE IN HERE
+    };
+
+    // TODO: `closeMoveModal()`
+    // Closes the 'Move To' modal for selecting a folder (i.e inbox, trash,
+    // etc.) to move a conversation/several conversations to.
+    //
+    // @post    : [success] the Move modal will be invisible and inactive
+    //
+    // @param   : null
+    // @return  : null
+    $scope.closeMoveModal = () => {
+        // CODE IN HERE
+    };
+
+    // TODO: `moveConvos(convoIds, dest)`
+    // Uses convoService to move the given conversations to the specified
+    // destination folder.
+    //
+    // @pre     : `convoIds` must be an array of valid conversation ids
+    // @pre     : `dest` must be a valid string corresponding to a destination
+    // folder to move the conversations to
+    // @post    : [success] the conversations will be moved to the given folder
+    // @post    : [error] the conversations will stay in their current
+    // respective folders
+    // @post    : [error] a notification will be logged with the error
+    //
+    // @param   : convoIds  : String[]  : id(s) of conversations to change
+    // the status of
+    // @param   : dest      : String    : the folder to move the
+    // conversations(s) to
+    // @return  : null
+    var moveConvos = (convoIds, dest) => {
+        // CODE IN HERE
+    };
+
+    ////////////////////////// VIEWING CONVERSATIONS ///////////////////////////
+
+    // TODO: `viewConvo(convoId)`
+    // Use stateProvider to show the given conversation on screen
+    //
+    // @pre     : a valid conversation id must be given
+    // @post    : [success] the conversation will be shown on screen via the
+    // convoPane
+    // @post    : [error] the currently viewed conversation will not change (if
+    // any)
+    // @post    : [error] an error notification will be logged and shown to the
+    // user
+    //
+    // @param   : convoId   : String    : the id of the conversation to view
+    // @return  : null
+    $scope.viewConvo = (convoId) => {
     };
 });
+}());
