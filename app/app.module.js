7aef89e14400e6366eef59555b5a783d5bbeabed
joshpaulchan <joshpaulchan@gmail.com>
2016-11-08 11:05:57 -0500

    Add main app module

--- app/app.module.js
+++ app/app.module.js
@@ -3,14 +3,18 @@
 "use strict";
 
 // Initialize app
-angular.module('wr', ['ui-router', 'wr.controllers', 'wr.services', 'wr.directives'])
-.config(function($urlRouterProvider) {
-    $urlRouterProvider.otherwise('/login');
+angular.module('wr', ['ui.router', 'wr.controllers', 'wr.services', 'wr.directives'])
+.config(function($locationProvider, $stateProvider) {
+    $locationProvider.html5Mode(true);
 
-})
-.run(['$rootScope', 'authService', function($rootScope, authService) {
-
-}]);
+    $stateProvider
+        .state('app', { url: "/", abstract: true })
+        .state('app.conversations', {
+            url: "",
+            templateUrl: "app/templates/conversations.html",
+            controller: "convoListController"
+        });
+});
 
 // Initialize modules
 angular.module('wr.controllers', []);
