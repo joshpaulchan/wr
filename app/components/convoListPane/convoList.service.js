eeac6bd2bea15026bc5f769069831db182ef089b
joshpaulchan <joshpaulchan@gmail.com>
2016-11-08 10:11:15 -0500

    Rename files to convoList; add convoList.service.js

---
+++ app/components/convoListPane/convoList.service.js
@@ -0,0 +1,11 @@
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
+
+// `convoListService`
+//
+app.module('wr.services')
+.service('convoListService', function($http) {
+
+});
+}());
