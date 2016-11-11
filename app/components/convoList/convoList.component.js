bcee5e9b7ef8f03375e4db16a2556ebc1ff69a56
joshpaulchan <joshpaulchan@gmail.com>
2016-11-11 12:00:25 -0500

    Change convoList into a component

---
+++ app/components/convoList/convoList.component.js
@@ -0,0 +1,12 @@
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
+
+// `convoList` COMPONENT
+//
+angular.module('wr.components')
+.component('convoList', {
+    templateUrl: 'client/app/components/convoList/convoList.view.html',
+    controller: 'convoList'
+});
+}());
