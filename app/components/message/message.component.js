d43728892a09e2eb06ca9dfe77e476216b07988f
joshpaulchan <joshpaulchan@gmail.com>
2016-12-15 12:34:10 -0500

    Add message component

---
+++ app/components/message/message.component.js
@@ -0,0 +1,11 @@
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
+
+// `convoItem`
+//
+angular.module('wr.components')
+.component('message', {
+    templateUrl: 'client/app/components/message/message.view.html'
+});
+}());
