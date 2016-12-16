82805dd587d1352dd3e1a936f3592980248e1808
joshpaulchan <joshpaulchan@gmail.com>
2016-12-16 17:15:09 -0500

    get controller working

--- app/shared/messageBox/messageBox.controller.js
+++ app/shared/messageBox/messageBox.controller.js
@@ -3,8 +3,51 @@
 "use strict";
 
 angular.module('wr.controllers')
-.controller('messageBox', function($scope, $state) {
-    $scope.message = "";
+.controller('messageBox', function($scope, textAngularManager) {
+    var $ctrl = this;
+    
+    //////////////////////////////// TEMPLATES ////////////////////////////////
 
+    $ctrl.openTemplateModal = function() {
+        // CODE HERE
+        console.log("opening template modal...");
+    };
+
+    $scope.closeTemplateModal = function() {
+        // CODE HERE
+        console.log("opening template modal...");
+    };
+
+    /////////////////////////////// textAngular ///////////////////////////////
+
+    // FIXME: there has to be a better way to add a tool to the tool bars (uniquely).
+    // \ not this though, the templating functions won't be available until this controller.
+    // $provide.decorator('taOptions', function(taRegisterTool, taOptions) {
+    //     // $delegate is the taOptions we are decorating
+    //     // register the tool with textAngular
+    //     taRegisterTool('colourRed', {
+    //         iconclass: "fa fa-square red",
+    //         action: function(){
+    //             this.$editor().wrapSelection('forecolor', 'red');
+    //         }
+    //     });
+    //     // add the button to the default toolbar definition
+    //     taOptions.toolbar[1].push('colourRed');
+    //     return taOptions;
+    // });
+    try {
+        textAngularManager.addTool('insertTemplate', {
+            display: "<button><i class='fa fa-file-text'></i>Template</button>",
+            action: $ctrl.openTemplateModal
+        });
+    } catch (e) { }
+
+    $ctrl.toolbars = [
+        ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'pre'],
+        ['bold', 'italics', 'underline', 'strikeThrough'],
+        ['ul', 'ol', 'indent', 'outdent'],
+        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+        ['insertTemplate']
+    ];
 });
 }());
