70c036c8e8cde310412ece92f1d500c58908d6aa
joshpaulchan <joshpaulchan@gmail.com>
2017-02-21 11:04:07 -0500

    Add skeleton for login

---
+++ app/components/login/login.component.js
@@ -0,0 +1,12 @@
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
+
+// `login`
+//
+angular.module('wr.components')
+.component('login', {
+    controller: 'login',
+    templateUrl: 'client/app/components/login/login.view.html',
+});
+}());
---
+++ app/components/login/login.controller.js
@@ -0,0 +1,13 @@
+// Written by Joshua Paul A. Chan
+(function() {
+"use strict";
+
+// `login`
+// Controls the login card (conversation list pane), allowing for logging in and
+// logging out.
+//
+angular.module('wr.controllers')
+.controller('login', ['$scope', '$state', '$authService', function($scope, $state, $authService) {
+
+}]);
+}());
